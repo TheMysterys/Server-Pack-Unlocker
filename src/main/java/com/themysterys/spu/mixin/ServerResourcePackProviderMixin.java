e9863fa6eac4cb8017e837ad37961d5d07e70fe9
TheMysterys <the-mystery@outlook.com>
2022-12-11 16:27:10 +1300

    Updated to 1.19.3

--- src/main/java/com/themysterys/spu/mixin/ServerResourcePackProviderMixin.java
+++ src/main/java/com/themysterys/spu/mixin/ServerResourcePackProviderMixin.java
@@ -8,9 +8,13 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(ServerResourcePackProvider.class)
 public class ServerResourcePackProviderMixin {
 
-    /*@ModifyArg(method = "loadServerPack", at = @At(value = "INVOKE", target = ""), index = 7)
+    @ModifyArg(
+            method = "loadServerPack(Ljava/io/File;Lnet/minecraft/resource/ResourcePackSource;)Ljava/util/concurrent/CompletableFuture;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourceType;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;"),
+            index = 7
+    )
     private boolean loadServerPack(boolean isPinned) {
         return false;
-    }*/
+    }
 
 }
