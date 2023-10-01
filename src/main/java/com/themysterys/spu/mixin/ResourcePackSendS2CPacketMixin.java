7cec76dd9d47f06aabacedc45a47505f88964308
Daniel Orr <orrdaniel73@gmail.com>
2023-10-01 04:48:56 +0100

    1.20.2

--- src/main/java/com/themysterys/spu/mixin/ResourcePackSendS2CPacketMixin.java
+++ src/main/java/com/themysterys/spu/mixin/ResourcePackSendS2CPacketMixin.java
@@ -2,9 +2,9 @@ package com.themysterys.spu.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
-import net.minecraft.network.listener.ClientPlayPacketListener;
-import net.minecraft.network.packet.c2s.play.ResourcePackStatusC2SPacket;
-import net.minecraft.network.packet.s2c.play.ResourcePackSendS2CPacket;
+import net.minecraft.network.listener.ClientCommonPacketListener;
+import net.minecraft.network.packet.c2s.common.ResourcePackStatusC2SPacket;
+import net.minecraft.network.packet.s2c.common.ResourcePackSendS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ResourcePackSendS2CPacket.class)
 public class ResourcePackSendS2CPacketMixin {
 
-    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V", at = @At("HEAD"), cancellable = true)
-    private void apply(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo ci) {
+    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientCommonPacketListener;)V", at = @At("HEAD"), cancellable = true)
+    private void apply(ClientCommonPacketListener clientCommonPacketListener, CallbackInfo ci) {
         ServerInfo serverInfo = MinecraftClient.getInstance().getCurrentServerEntry();
         if (serverInfo != null && serverInfo.getResourcePackPolicy() == ServerInfo.ResourcePackPolicy.DISABLED) {
             if (MinecraftClient.getInstance().getNetworkHandler() != null) {
