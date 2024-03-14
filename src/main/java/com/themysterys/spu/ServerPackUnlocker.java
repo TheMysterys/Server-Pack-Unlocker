2e3661bd6f37e6c34b65748c4044e8cad1dc2b62
Kasa <1048064671@qq.com>
2024-03-14 20:05:27 +0800

    update to 1.20.4

--- src/main/java/com/themysterys/spu/ServerPackUnlocker.java
+++ src/main/java/com/themysterys/spu/ServerPackUnlocker.java
@@ -1,50 +1,9 @@
 package com.themysterys.spu;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.resource.ResourcePackProfile;
-
 public class ServerPackUnlocker implements ModInitializer {
-
-    private static ServerPackUnlocker instance;
-    private PackManager packManager;
-    private ResourcePackProfile currentServerPack;
-
-    private boolean modifiedPacks = false;
-
-    private PlayerConnectionEventHandler playerConnectionEventHandler;
 
     @Override
     public void onInitialize() {
-        instance = this;
-        packManager = new PackManager();
-
-        playerConnectionEventHandler = new PlayerConnectionEventHandler();
-        ClientPlayConnectionEvents.JOIN.register(playerConnectionEventHandler);
-        ClientPlayConnectionEvents.DISCONNECT.register(playerConnectionEventHandler);
-    }
-
-    public static PackManager getPackManager() {
-        return instance.packManager;
-    }
-
-    public static ResourcePackProfile getCurrentServerPack() {
-        return instance.currentServerPack;
-    }
-
-    public static void setCurrentServerPack(ResourcePackProfile currentServerPack) {
-        instance.currentServerPack = currentServerPack;
-    }
-
-    public static String getCurrentServerAddress() {
-        return instance.playerConnectionEventHandler.currentServerAddress;
-    }
-
-    public static boolean hasModifiedPacks() {
-        return instance.modifiedPacks;
-    }
-
-    public static void setModifiedPacks(boolean modifiedPacks) {
-        instance.modifiedPacks = modifiedPacks;
     }
 }
