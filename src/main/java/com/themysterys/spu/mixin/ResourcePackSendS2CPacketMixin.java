f2e5022823b697897dd4056995907642a2d76706
TheMysterys <the-mystery@outlook.com>
2024-05-30 13:52:08 +1200

    backport to 1.17 -> 1.20.1

--- src/main/java/com/themysterys/spu/mixin/ResourcePackSendS2CPacketMixin.java
+++ src/main/java/com/themysterys/spu/mixin/ResourcePackSendS2CPacketMixin.java
@@ -2,29 +2,23 @@ package com.themysterys.spu.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ServerInfo;
-import net.minecraft.network.listener.ClientCommonPacketListener;
-import net.minecraft.network.packet.c2s.common.ResourcePackStatusC2SPacket;
-import net.minecraft.network.packet.s2c.common.ResourcePackSendS2CPacket;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.c2s.play.ResourcePackStatusC2SPacket;
+import net.minecraft.network.packet.s2c.play.ResourcePackSendS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.UUID;
-
 @Mixin(ResourcePackSendS2CPacket.class)
 public abstract class ResourcePackSendS2CPacketMixin {
 
-    @Shadow
-    public abstract UUID id();
-
-    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientCommonPacketListener;)V", at = @At("HEAD"), cancellable = true)
-    private void apply(ClientCommonPacketListener clientCommonPacketListener, CallbackInfo ci) {
+    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V", at = @At("HEAD"), cancellable = true)
+    private void apply(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo ci) {
         ServerInfo serverInfo = MinecraftClient.getInstance().getCurrentServerEntry();
         if (serverInfo != null && serverInfo.getResourcePackPolicy() == ServerInfo.ResourcePackPolicy.DISABLED) {
             if (MinecraftClient.getInstance().getNetworkHandler() != null) {
-                MinecraftClient.getInstance().getNetworkHandler().sendPacket(new ResourcePackStatusC2SPacket(this.id(), ResourcePackStatusC2SPacket.Status.SUCCESSFULLY_LOADED));
+                MinecraftClient.getInstance().getNetworkHandler().sendPacket(new ResourcePackStatusC2SPacket(ResourcePackStatusC2SPacket.Status.SUCCESSFULLY_LOADED));
             }
             ci.cancel();
         }
